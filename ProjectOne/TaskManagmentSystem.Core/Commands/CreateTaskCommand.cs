d71aa88c66b2aca10c01102171fbbce4e54865b3
Georgi Petrov <georgi.petrov1820@abv.bg>
2021-07-30 13:15:48 +0300

    Save changes to repo

--- ProjectOne/TaskManagmentSystem.Core/Commands/CreateTaskCommand.cs
+++ ProjectOne/TaskManagmentSystem.Core/Commands/CreateTaskCommand.cs
@@ -1,11 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Reflection;
 using TaskManagmentSystem.Core.Contracts;
+using TaskManagmentSystem.Models.Common;
+using TaskManagmentSystem.Models.Contracts;
 
 namespace TaskManagmentSystem.Core.Commands
 {
     public class CreateTaskCommand : BaseCommand
     {
+        private const int numberOfParameters = 3;
         public CreateTaskCommand(IList<string> commandParameters, IRepository repository)
             : base(commandParameters, repository)
         {
@@ -13,7 +17,26 @@ namespace TaskManagmentSystem.Core.Commands
         }
         public override string Execute()
         {
-            throw new NotImplementedException();
+            if (CommandParameters.Count != numberOfParameters)
+            {
+                throw new UserInputException("Invalid number of parameters"); //change later in constants
+            }
+            string itemType = CommandParameters[0];
+            string itemName = CommandParameters[1];
+            string itemDescription = CommandParameters[2];
+            string namespaceName = Assembly.GetExecutingAssembly().GetName().Name;
+            string fullType = $"{namespaceName}.{itemType}";
+            Type type;
+            try
+            {
+                type = Type.GetType(namespaceName, true, true);
+            }
+            catch (Exception)
+            {
+                throw new UserInputException("BoardItem of that type doesnt exist");  // change later in constants              
+            }
+            //IBoardItem item = this.Repository.CreateTask();
+            return $"{itemType} was created";
         }
     }
 }

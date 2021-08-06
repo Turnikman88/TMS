5fbd207840a242ee7c398e4272e14b580b81c389
Kalin Balimezov <kbalimezov@gmail.com>
2021-08-06 23:48:08 +0300

    Merging last changes

--- ProjectOne/TaskManagmentSystem.Core/Commands/AddCommentToTask.cs
+++ ProjectOne/TaskManagmentSystem.Core/Commands/AddCommentToTask.cs
@@ -20,20 +20,9 @@ namespace TaskManagmentSystem.Core.Commands
             Validator.ValidateParametersCount(numberOfParameters, CommandParameters.Count);
 
             string teamName = CommandParameters[0];
-            //string boardName = CommandParameters[1];
             int itemID = int.Parse(CommandParameters[1]);
             string content = CommandParameters[2];
             string author = CommandParameters[3];
-
-            // var team = this.Repository.GetTeam(teamName) ?? throw new UserInputException(string.Format(Constants.TEAM_DOESNT_EXSIST, teamName));
-            // var board = team.Boards.FirstOrDefault(x => x.Name == boardName) ?? throw new UserInputException(string.Format(Constants.BOARD_DOESNT_EXSIST, boardName));
-            // var item = board.Tasks.FirstOrDefault(x => x.Id == itemID) ?? throw new UserInputException(string.Format(Constants.TASK_DOESNT_EXSIST, itemID));
-            //  var user = this.Repository.FindUserByName(author) ?? throw new UserInputException(string.Format(Constants.USER_DOESNT_EXSIST, author));
-
-            //if (this.Repository.IsTeamMember(team, user))
-            // {
-            //    throw new UserInputException(string.Format(Constants.MEMBER_NOT_IN_TEAM, author));
-            // }
 
             if (!this.Repository.IsTeamMember(this.Repository.GetTeam(teamName), this.Repository.LoggedUser))
             {

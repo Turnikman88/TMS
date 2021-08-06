2ea5db044d62e969e49f3c42c229e3a6cf72235e
Kalin Balimezov <kbalimezov@gmail.com>
2021-08-06 23:56:03 +0300

    cm

--- ProjectOne/TaskManagmentSystem.Core/Commands/AddCommentToTask.cs
+++ ProjectOne/TaskManagmentSystem.Core/Commands/AddCommentToTask.cs
@@ -25,15 +25,6 @@ namespace TaskManagmentSystem.Core.Commands
             string content = CommandParameters[2];
             string author = CommandParameters[3];
 
-            // var team = this.Repository.GetTeam(teamName) ?? throw new UserInputException(string.Format(Constants.TEAM_DOESNT_EXSIST, teamName));
-            // var board = team.Boards.FirstOrDefault(x => x.Name == boardName) ?? throw new UserInputException(string.Format(Constants.BOARD_DOESNT_EXSIST, boardName));
-            // var item = board.Tasks.FirstOrDefault(x => x.Id == itemID) ?? throw new UserInputException(string.Format(Constants.TASK_DOESNT_EXSIST, itemID));
-            //  var user = this.Repository.FindUserByName(author) ?? throw new UserInputException(string.Format(Constants.USER_DOESNT_EXSIST, author));
-
-            //if (this.Repository.IsTeamMember(team, user))
-            // {
-            //    throw new UserInputException(string.Format(Constants.MEMBER_NOT_IN_TEAM, author));
-            // }
 
             if (!this.Repository.IsTeamMember(this.Repository.GetTeam(teamName), this.Repository.LoggedUser))
             {

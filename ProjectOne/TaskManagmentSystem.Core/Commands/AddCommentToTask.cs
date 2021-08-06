d30d5f4654a7eaee6e5319f4d29f221638ad2249
Georgi Petrov <georgi.petrov1820@abv.bg>
2021-08-07 00:02:21 +0300

    conflicts

--- ProjectOne/TaskManagmentSystem.Core/Commands/AddCommentToTask.cs
+++ ProjectOne/TaskManagmentSystem.Core/Commands/AddCommentToTask.cs
@@ -14,21 +14,7 @@ namespace TaskManagmentSystem.Core.Commands
 
         public override string Execute()
         {
-            Validator.ValidateParametersCount(numberOfParameters, CommandParameters.Count);
-
-            string teamName = CommandParameters[0];
-            int itemID = int.Parse(CommandParameters[1]);
-            string content = CommandParameters[2];
-            string author = CommandParameters[3];
-
-            if (!this.Repository.IsTeamMember(this.Repository.GetTeam(teamName), this.Repository.LoggedUser))
-            {
-                throw new UserInputException(string.Format(Constants.MEMBER_NOT_IN_TEAM, this.Repository.LoggedUser.Name));
-            }
-            var task = this.Repository.FindTaskByID(itemID) ?? throw new UserInputException("Task doesn't exsist");
-
-            task.AddComment(new Comment(content, author));
-            return $"Comment was added";
+            throw new NotImplementedException();
         }
     }
 }
